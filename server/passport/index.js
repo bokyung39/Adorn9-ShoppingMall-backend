25d469f88a02b60c6a88a3a67bffa5ecc7092142
kyung <bkykim12@gmail.com>
2023-10-09 01:59:37 +0900

    Revert "jwt 구현"
    This reverts commit 0f25fabdb2fbaf3389825c1184ce92fc470ddeb2.

--- server/passport/index.js
+++ server/passport/index.js
@@ -1,29 +1,23 @@
 const passport = require('passport');
 const { User } = require('../models');
 const local = require('./strategies/local');
-const kakao = require('./strategies/kakaoStrategy');
-const jwt = require('./strategies/jwt');
+
 module.exports = () => {
+  passport.use(local);
 
-  // passport.serializeUser((user, done) => {
-  //   done(null, user.id);
-  // });
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
 
-  // // 서버에 요청이 있을 때마다 호출됨
-  // // done 의 두 번째 인자로 user를 전달하게 되면 req.user로 user의 값을 접근할 수 있게 됨
-  //  passport.deserializeUser(async (id, done) => {
-  //   try {
-  //     const user = await User.findOne({ id: id });
-  //     if (!user) {
-  //       return done(null, false);
-  //     }
-  //     done(null, user);
-  //   } catch (err) {
-  //     done(err, null);
-  //   }
-  // });
-  passport.use('local', local);
-  passport.use(jwt);
-  passport.use('kakao', kakao);
-  
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findOne({ id });
+      if (!user) {
+        return done(null, false);
+      }
+      done(null, user);
+    } catch (err) {
+      done(err, null);
+    }
+  });
 }
--- server/routes/loginRouter.js
+++ server/routes/loginRouter.js
@@ -1,48 +1,32 @@
 const { Router } = require('express');
 const passport = require('passport'); 
-const {setUserToken} = require('../utils/jwt'); 
-const router = Router();
 const asyncHandler = require('../utils/async-handler');
+const router = Router();
 /*
 로그인 성공 데이터
 {
-  "email": "aaaaa@example.com",
-  "password": "1234!@asd56"
+  "email": "usera@example.com",
+  "password": "1234"
 }
 hashpassword: $2b$08$AN10tp5cY4O6kdE4i8a9DukYylAs0O/hoC5.VwPVy2WMIOk4mogtK
 */
-
-
-router.post('/login', passport.authenticate('local', { session: false }), asyncHandler((req, res) => {
-  //throw{status:400, message:"throw"};
-  setUserToken(res, req.user);
+router.post('/login', passport.authenticate('local'), asyncHandler(async (req, res) => {
+  // 로그인 성공 시 처리
   res.status(200).json({
     message: '로그인 성공'
   });
 }));
 
-
-// router.post('/login', (req, res, next) => {
-//   passport.authenticate('local', { session: false }, (err, user, info) => {
-//     if (err) {
-//       console.error('로그인 중 오류 발생:', err);
-//       return next(err); // 에러를 에러 핸들러로 넘김
-//     }
-//     setUserToken(res, req.user);
-//     res.status(200).json({ message: '로그인 성공' });
-//   })(req, res, next);
-// });
-
-
+// 로그아웃
 router.get('/logout', (req, res) => {
-  try {
-    // 쿠키를 삭제하기 위해 clearCookie() 메서드 사용
-    res.clearCookie('token'); // 'token'은 삭제하려는 쿠키의 이름
-    return res.status(202).json({ message: '로그아웃 성공' });
-  } catch (error) {
-    console.error('로그아웃 중 오류 발생:', error);
-    return res.status(500).json({ message: '로그아웃 중 오류가 발생했습니다.' });
-  }
+  req.logout((err) => {
+    if (err) {
+      return res.status(500).json({ message: '로그아웃 중 오류가 발생했습니다.' });
+    }
+    res.status(202).json({
+      message: '로그아웃 성공'
+    });
+  });
 });
 
 module.exports = router;
