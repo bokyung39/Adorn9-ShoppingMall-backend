e04fbfe3202221d454b9a4d5515bc96f1f458ca8
dcmailreceiver@gmail.com <dcmailreceiver@gmail.com>
2023-10-10 19:44:31 +0900

    merge 할래요

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,7 +2,9 @@ const {Router } = require ('express');
 const asyncHandler = require('../utils/async-handler');
 const {userService} = require('../services/userService')
 const router = Router();
-const loginRequired = require('../middlewares/login-required')
+const passport = require('passport'); 
+const loginRequired = require('../middlewares/login-required');
+const { User } = require('../models');
 
 //회원가입
 
@@ -59,16 +61,45 @@ router.get('/profile', asyncHandler(async(req,res,next)=>{
 router.delete('/withdraw', asyncHandler(async(req,res,next)=>{
 
   const {email, user_name}= req.body;
+  const goodbye_user = await User.findOne({email})
   await userService.resignUser(email);
-  res.status(200).json({status:200, msg:`${user_name}님의 탈퇴가 완료됐습니다.`})
+  res.status(200).json({status:200, msg:`${goodbye_user.user_name}님의 탈퇴가 완료됐습니다.`})
 }))
 
 //비밀번호 찾기
-router.post('/changing-password', asyncHandler(async(req,res,next)=>{
+router.post('/reset-password', asyncHandler(async(req,res,next)=>{
   const {email} = req.body;
+  const user = await findOne({email})
+  if(!user) {throw new Error('등록된 계정이 아닙니다.')}
   const goodbye = await userService.passwordReset(email)
   res.status(200).send({status:200, msg:'변경된 비밀번호를 이메일로 발송했습니다.'})
 }))
+
+router.post('/changing-password',asyncHandler(async(req,res,next)=>{
+  res.render('')
+}))
+
+
+
+
+router.post('/login', passport.authenticate('local'), asyncHandler(async (req, res) => {
+  // 로그인 성공 시 처리
+  res.status(200).json({
+    message: '로그인 성공'
+  });
+}));
+
+router.get('/logout', (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      return res.status(500).json({ message: '로그아웃 중 오류가 발생했습니다.' });
+    }
+    res.status(202).json({
+      message: '로그아웃 성공'
+    });
+  });
+});
+
 //주문 목록 조회
 
 // router.get('/order-list/?user_name',asyncHandler(async(req,res,next)=>{
